e3037ea936589b4380ccbe45484ea67a48814daf
Joe Boyle <joboyle@wayfair.com>
2020-06-10 10:04:15 -0400

    Tweaking configs

--- frontend/.babelrc.js
+++ frontend/.babelrc.js
@@ -2,7 +2,6 @@ const makeBabelConfig = require('babel-react-simple');
 
 const config = makeBabelConfig();
 
-// config.plugins.push('istanbul');
-// config.env = { test: { plugins: ['istanbul'] } };
+config.plugins.push('istanbul');
 
 module.exports = config;
