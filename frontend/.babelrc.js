8ad4bbc9c52a1fa0ebb36e2bc26570162db4c17c
Joe Boyle <joboyle@wayfair.com>
2020-06-10 11:22:00 -0400

    Get cypress coverage working

--- frontend/.babelrc.js
+++ frontend/.babelrc.js
@@ -1,7 +1,6 @@
 const makeBabelConfig = require('babel-react-simple');
 
 const config = makeBabelConfig();
-
 config.plugins.push('istanbul');
 
 module.exports = config;
--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -20,6 +20,6 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 
   require('@cypress/code-coverage/task')(on, config);
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+
   return config;
 };
